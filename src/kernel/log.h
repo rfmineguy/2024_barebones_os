1f67007fb15e35f169bd1f8e73a1b4204530d5d3
rfmineguy <rfmineguy@gmail.com>
2024-11-01 17:19:03 -0700

    Log: Fixed name of log_err

--- src/kernel/log.h
+++ src/kernel/log.h
@@ -4,7 +4,7 @@
 
 #define log_info(cat, fmt, ...) serial_printf("[" cat "][INFO] : " fmt, ##__VA_ARGS__)
 #define log_warn(cat, fmt, ...) serial_printf("[" cat "][WARN] : " fmt, ##__VA_ARGS__)
-#define  log_err(cat, fmt, ...) serial_printf("[" cat "][ERR ] : " fmt, ##__VA_ARGS__)
+#define log_err(cat, fmt, ...) serial_printf("[" cat "][ERR ] : " fmt, ##__VA_ARGS__)
 #define log_crit(cat, fmt, ...) serial_printf("[" cat "][CRIT] : " fmt, ##__VA_ARGS__)
 
 #endif
