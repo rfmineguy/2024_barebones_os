0eae51d96fd5cdb937b6618f76f9b40307b369e2
rfmineguy <rfmineguy@gmail.com>
2024-11-01 17:18:22 -0700

    Error: Added global error management

---
+++ src/kernel/error.c
@@ -0,0 +1,21 @@
+#include "error.h"
+
+#define ERROR_MAP_SIZE 10
+
+char* error_to_str_map[ERROR_MAP_SIZE] = {
+    [0] = "No error",
+    [1] = "Generic error",
+    [2] = "Invalid shell command",
+    [3] = "Invalid",
+    [4] = "Invalid",
+    [5] = "Invalid",
+    [6] = "Invalid",
+    [7] = "Invalid",
+    [8] = "Invalid",
+    [9] = "Invalid",
+};
+
+char* map_error_code(int code) {
+    if (code < 0 || code >= ERROR_MAP_SIZE) return "Out of bounds code";
+    return error_to_str_map[code];
+}
---
+++ src/kernel/error.h
@@ -0,0 +1,11 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#define ERROR_NONE 0
+#define ERROR_GENERIC 1
+#define ERROR_INVALID_CMD 2
+#define ERROR_INVALID 3
+
+char* map_error_code(int);
+
+#endif
