f73c7643aa186d8ee6a329b941206ce0734ec3c0
rfmineguy <rfmineguy@gmail.com>
2024-11-09 01:28:15 -0800

    Splash: Adjust the splash to utilize v2 of the ui

--- src/kernel/rfos_splash.c
+++ src/kernel/rfos_splash.c
@@ -2,6 +2,15 @@
 #include "ui.h"
 #define R(x) #x
 
+#define REP0(x)
+#define REP1(x) (x)
+#define REP2(x) REP1(x), (x)
+#define REP3(x) REP2(x), (x)
+#define REP4(x) REP3(x), (x)
+#define REP5(x) REP4(x), (x)
+#define REP6(x) REP5(x), (x)
+#define REP7(x) REP6(x), (x)
+
 //       R       F       O        S
 //    |-----| |-----| |------| |-----|
 // ╔═════════════════════════════════════╗
@@ -11,18 +20,18 @@
 // ║  ██   ██ ██      ██    ██      ██   ║
 // ║  ██   ██ ██       ██████  ███████   ║
 // ╚═════════════════════════════════════╝
-void rfos_splash(ui_box* box) {
-
-    // ui_box_emit(E(2, ' '), E(6, BLCK), E(2, ' '));
-
-
-    emit(3,  E(1,CTL),  E(37,HOR), E(1, CTR));
-    //                                        R                                       F                                 O                                   S
-    //                             |--------------------------------|            |---------|            |--------------------------------|             |---------|           
-    emit(11, E(1,VERT), E(2, ' '), E(6, BLCK),            E(2, ' '),             E(7, BLCK), E(2, ' '),             E(6, BLCK),             E(2, ' '), E(7, BLCK), E(3, ' '),  E(1, VERT));
-    emit(15, E(1,VERT), E(2, ' '), E(2, BLCK), E(3, ' '), E(2, BLCK), E(1, ' '), E(2, BLCK), E(6, ' '), E(2, BLCK), E(4, ' '),  E(2, BLCK), E(1, ' '), E(2, BLCK), E(8, ' '),  E(1, VERT));
-    emit(13, E(1,VERT), E(2, ' '), E(6, BLCK),            E(2, ' '),             E(5, BLCK), E(3, ' '), E(2, BLCK), E(4, ' '),  E(2, BLCK), E(1, ' '), E(7, BLCK), E(3, ' '),  E(1, VERT));
-    emit(16, E(1,VERT), E(2, ' '), E(2, BLCK), E(3, ' '), E(2, BLCK), E(1, ' '), E(2, BLCK), E(6, ' '), E(2, BLCK), E(4, ' '),  E(2, BLCK), E(1, ' '), E(5, ' '),  E(2, BLCK), E(3, ' '), E(1, VERT));
-    emit(13, E(1,VERT), E(2, ' '), E(2, BLCK), E(3, ' '), E(2, BLCK), E(1, ' '), E(2, BLCK), E(7, ' '),             E(6, BLCK),             E(2, ' '), E(7, BLCK), E(3, ' '),  E(1, VERT));
-    emit(3,  E(1,CBL),  E(37,HOR), E(1, CBR));
+void rfos_splash(ui_box2* box) {
+    ////                            R                     Space        F         Space                     O                   Space          S                                  S
+    ////             |--------------------------------| |-------|  |--------|  |-------|  |--------------------------------| \-------|   |---------|           
+    char line1[] = { REP6(BLCK),                        REP2(' '), REP7(BLCK), REP2(' '),             REP6(BLCK),            REP2(' '), REP7(BLCK), 0};
+    char line2[] = { REP2(BLCK), REP3(' '), REP2(BLCK), REP1(' '), REP2(BLCK), REP6(' '), REP2(BLCK), REP4(' '), REP2(BLCK), REP1(' '), REP2(BLCK), 0};
+    char line3[] = { REP6(BLCK),                        REP2(' '), REP5(BLCK), REP3(' '), REP2(BLCK), REP4(' '), REP2(BLCK), REP1(' '), REP7(BLCK), 0};
+    char line4[] = { REP2(BLCK), REP3(' '), REP2(BLCK), REP1(' '), REP2(BLCK), REP6(' '), REP2(BLCK), REP4(' '), REP2(BLCK), REP1(' '), REP5(' '), REP2(BLCK), 0};
+    char line5[] = { REP2(BLCK), REP3(' '), REP2(BLCK), REP1(' '), REP2(BLCK), REP7(' '),             REP6(BLCK),            REP2(' '), REP7(BLCK), 0};
+    ui2_putstr(box, 2, 1, line1);
+    ui2_putstr(box, 2, 2, line2);
+    ui2_putstr(box, 2, 3, line3);
+    ui2_putstr(box, 2, 4, line4);
+    ui2_putstr(box, 2, 5, line5);
+    ui2_putstr(box, 2, 7, "Welcome to RFOS!!!");
 }
--- src/kernel/rfos_splash.h
+++ src/kernel/rfos_splash.h
@@ -1,8 +1,8 @@
 #ifndef RFOS_SPLASH_H
 #define RFOS_SPLASH_H
 #include "extascii.h"
-#include "ui.h"
+#include "ui_v2.h"
 
-void rfos_splash(ui_box*);
+void rfos_splash(ui_box2*);
 
 #endif
