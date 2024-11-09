a6171659d7dc193162756c08877f9b1b99ee4e9d
rfmineguy <rfmineguy@gmail.com>
2024-11-08 22:01:08 -0800

    Shell: Worked on the shell, added result concept for commands, etc

--- src/kernel/shell.c
+++ src/kernel/shell.c
@@ -8,9 +8,9 @@
 #include "keyboard.h"
 #include "error.h"
 #include "fat.h"
-#include "arena.h"
+#include "ui.h"
 
-bool return_pressed = false;
+bool return_pressed = false, backspace_pressed = false;
 char shell_buffer[1024] = {0};
 int shell_buffer_i = 0;
 arena* kernel_arena = (void*)0;
@@ -25,6 +25,7 @@ int shell_keyboard_listener(char ch) {
                   if (shell_buffer_i <= 0) {
                       return ch;
                   }
+                  backspace_pressed = true;
                   shell_buffer_i--;
                   shell_buffer[shell_buffer_i] = 0;
                   break;
@@ -42,33 +43,58 @@ int shell_keyboard_listener(char ch) {
 }
 
 // shell runs in kernel level. just for simplicity
-int shell_run(arena* _kernel_arena) {
+int shell_run(arena* _kernel_arena, ui_box* box) {
     kernel_arena = _kernel_arena;
     int exit_code = 0;
+    struct builtin_result r;
+    ui_box_clear(box);
+
     while (1) {
-        while (!return_pressed) {
-            vga_clearline();
-            size_t x, y;
-            vga_save_cursor(&x, &y);
-            vga_writestring("> ");
-            vga_write(shell_buffer, 80);
-            vga_put_cursor_at(x, y);
-        }
+        ui_box_draw(box);
+
+        // perform some logic when enter is pressed
         if (return_pressed) {
             return_pressed = false;
-            vga_putch('\n');
+            box->cursorx = 0;
+            box->cursory++;
+            ui_box_clearregion(box, 1, box->cursory, 3 + strlen(shell_buffer), 1);
+
             // process command
             log_info("ShellRun", "Processing '%s'\n", shell_buffer);
-            int ret = shell_process(shell_buffer);
-            if (ret > 0) {
-                log_err("ShellRun", "%s\n", map_error_code(ret));
-                k_printf("%s\n", map_error_code(ret));
+            r = shell_process(shell_buffer);
+            if (r.code > 0) {
+                log_err("ShellRun", "%s\n", map_error_code(r.code));
+                ui_box_printf(box, box->cursorx, box->cursory, "!!%s\n", map_error_code(r.code));
             }
+            else {
+                log_info("ShellRun", "Command ran successfully\n");
+                if (r.string_result)
+                    ui_box_printf(box, box->cursorx, box->cursory, "%s\n", r.string_result);
+            }
+            box->cursory++;
+            if (box->cursory >= box->y + box->h) box->cursory = 1;
 
             // clear buffer
             shell_buffer_i = 0;
             shell_buffer[0] = 0;
             for (int i = 0; i < 80; i++) shell_buffer[i] = 0;
+        }
+
+        // display state of current shell line
+        while (!return_pressed) {
+            // scroll buffer
+            if (box->cursory >= box->h - 4) {
+                // ui_box_scroll_vertical(box);
+                ui_box_scroll_vertical(box);
+                box->cursory-=1;
+            }
+            size_t x, y;
+            if (backspace_pressed) {
+                ui_box_clearregion(box, 1, box->cursory, 3 + strlen(shell_buffer), 1);
+                backspace_pressed = false;
+            }
+            ui_box_printf(box, 0, box->cursory, "> ");
+            ui_box_printf(box, box->cursorx, box->cursory, "%s", shell_buffer);
         }
     }
     return exit_code;
@@ -91,7 +117,7 @@ void tokenize_args(char* str, struct argument_ctx* ctx) {
 
 // shell command format
 //    <command> <args(space separated)>
-int shell_process(const char* buf) {
+struct builtin_result shell_process(const char* buf) {
     struct argument_ctx ctx = {0};
     tokenize_args(buf, &ctx);
 
@@ -101,34 +127,38 @@ int shell_process(const char* buf) {
     if (strcmp(ctx.args[0], "read") == 0) return shell_read_builtin(&ctx);
     if (strcmp(ctx.args[0], "dir") == 0)  return shell_dir_builtin(&ctx);
 
-    return ERROR_INVALID_CMD; // invalid command supplied
+    return BUILTIN_RESULT(ERROR_INVALID_CMD, (void*)0); // invalid command supplied
 }
 
 // shell command
 //    read <filename>
 //    
 //    <filename> should be converted to 8.3 format for fat12
-//      i.e.   "file.txt" -> "FILE    TXT" 
+//      i.e.   "file.txt" -> "FILE    TXT"
 // return:
 //   0 = success
 //   1 = file not found
 //   2 = failed to read
-int shell_read_builtin(const struct argument_ctx* arg_ctx) {
+struct builtin_result shell_read_builtin(const struct argument_ctx* arg_ctx) {
     log_info("ShellBuiltin", "Read file\n");
     char name_8_3[12] = "           ";
     if (fat_filename_to_8_3(arg_ctx->args[1], name_8_3) == -1) {
-        k_printf("  !!Error %d\n");
-        return ERROR_GENERIC;
+        return BUILTIN_RESULT(ERROR_GENERIC, (void*)0);
     }
     log_info("ShellBuiltin", "%s\n", name_8_3);
     dir_entry* f = (void*)0;
     if (!(f = fat_find_file(name_8_3))) {
-       return ERROR_FILE_NOT_FOUND;
+        return BUILTIN_RESULT(ERROR_FILE_NOT_FOUND, (void*)0);
     }
 
     uint8_t* buf = fat_read_entry(f, kernel_arena);
-    if (!buf) log_info("ShellBuiltin", "Failed to read file\n");
+    if (!buf) {
+        log_info("ShellBuiltin", "Failed to read file\n");
+        return BUILTIN_RESULT(ERROR_FILE_READ, (void*)0);
+    }
+    
     else {
+        return BUILTIN_RESULT(ERROR_NONE, (char*)buf);
         for (size_t i = 0; i < f->Size; i++) {
             if (isprint(buf[i])) {
                 serial_printf("%c", buf[i]);
@@ -142,11 +172,11 @@ int shell_read_builtin(const struct argument_ctx* arg_ctx) {
         k_printf("\n");
     }
 
-    return ERROR_NONE;
+    return BUILTIN_RESULT(ERROR_GENERIC, (void*)0);
 }
 
-int shell_dir_builtin(const struct argument_ctx* arg_ctx) {
+struct builtin_result shell_dir_builtin(const struct argument_ctx* arg_ctx) {
     (void)arg_ctx;
     log_info("ShellBuiltin", "Dir\n");
-    return ERROR_NONE;
+    return BUILTIN_RESULT(ERROR_NONE, (void*)0);
 }
--- src/kernel/shell.h
+++ src/kernel/shell.h
@@ -1,16 +1,26 @@
 #ifndef SHELL_H
 #define SHELL_H
+#include "arena.h"
+#include "ui.h"
 
 struct argument_ctx {
     char* args[30];
     int arg_counter;
 };
 
-int shell_keyboard_listener(char);
-int shell_run();
-int shell_process(const char*);
+struct builtin_result {
+    int code;
+    char* string_result;
+};
+#define BUILTIN_RESULT(_code, _str) (struct builtin_result) {.code=_code,.string_result=_str}
 
-int shell_read_builtin(const struct argument_ctx*);
-int shell_dir_builtin(const struct argument_ctx*);
+int shell_keyboard_listener(char);
+
+int shell_print_result(char*);
+int shell_run(arena*, ui_box*);
+struct builtin_result shell_process(const char*);
+
+struct builtin_result shell_read_builtin(const struct argument_ctx*);
+struct builtin_result shell_dir_builtin(const struct argument_ctx*);
 
 #endif
