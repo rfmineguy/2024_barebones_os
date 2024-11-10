2d4e3436f76cecae84d43dffaa9c01b641cbcc82
rfmineguy <rfmineguy@gmail.com>
2024-11-09 20:53:04 -0800

    Memory: suppress unused variable warning

--- src/kernel/memory.c
+++ src/kernel/memory.c
@@ -5,8 +5,7 @@
 void memory_init(struct multiboot_info* info) {
     for (uint32_t i = 0; i < info->mmap_length; i += sizeof(struct multiboot_mmap_entry)) {
         struct multiboot_mmap_entry *mmmt = (struct multiboot_mmap_entry*)(info->mmap_addr+i);
+        (void)mmmt;
 
-        log_info("MemInit ", "loaddr: %x, hiaddr: %x, lolen: %x, hilen: %x, size: %x, type: %d\n",
-                mmmt->addrlo, mmmt->addrhi, mmmt->lenlo, mmmt->lenhi, mmmt->size, mmmt->type);
     }
 }
