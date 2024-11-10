331adeb95c86aab4982be38ce26f178020ed15a6
rfmineguy <rfmineguy@gmail.com>
2024-11-09 20:52:29 -0800

    UI: Wrote the code for rfos splash screen, and tip splash screen

---
+++ src/kernel/tips.c
@@ -0,0 +1,5 @@
+#include "tips.h"
+
+void tips_populate(ui_box2* box) {
+    ui2_putstr(box, 1, 0, "read <file>");
+}
---
+++ src/kernel/tips.h
@@ -0,0 +1,7 @@
+#ifndef TIPS_H
+#define TIPS_H
+#include "ui_v2.h"
+
+void tips_populate(ui_box2*);
+
+#endif
