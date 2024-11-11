967bea2711112f782312ab45d09e090910e1a6c5
Riley Fischer <rfmineguy@gmail.com>
2024-11-11 09:04:17 -0800

    Build: Added a powershell script (build.ps1) that mirrors the functionality of the unix shell script (build.sh) for use on Windows

---
+++ scripts/build.ps1
@@ -0,0 +1,73 @@
+#!/usr/bin/env pwsh
+
+# Define the Docker command (adjusted for PowerShell)
+$docker_cmd = "docker run --rm -it -w /root/workspace -v $(Get-Location):/root/workspace debian-test"
+
+function usage {
+    Write-Host "usage: build <subcommand>"
+    Write-Host "Subcommands:"
+    Write-Host "  docker_get          : Build the required docker container"
+    Write-Host "  build               : Use the container to build the kernel"
+    Write-Host "  clean               : Use the container to clean the build files"
+    Write-Host "  checkmboot          : Check if the generated binary is multiboot"
+    Write-Host "  qemu                : Run generated kernel in qemu"
+    Write-Host "  help                : Display this menu"
+    Write-Host "$docker_cmd"
+}
+
+function handle_build {
+    New-Item -ItemType Directory -Force -Path out
+    $params = 'sh -c "make build -f ./scripts/debian.Makefile"'
+    Write-Host "$docker_cmd $params"
+    Invoke-Expression "$docker_cmd $params"
+}
+
+function handle_clean {
+    $params = 'sh -c "make clean -f ./scripts/debian.Makefile"'
+    Write-Host "$docker_cmd $params"
+    Invoke-Expression "$docker_cmd $params"
+}
+
+function handle_checkmboot {
+    $params = 'sh -c "make grub_check_multiboot -f scripts/debian.Makefile"'
+    Write-Host "$docker_cmd $params"
+    Invoke-Expression "$docker_cmd $params"
+}
+
+function handle_docker_get {
+    Set-Location -Path 'docker'
+    docker build -f debian.Dockerfile -t debian-test . --platform=linux/amd64
+}
+
+function handle_qemu {
+    # qemu-system-i386 -drive file=out/main.img,format=raw -cdrom out/os.iso -boot d -vga std -serial file:output.txt
+    qemu-system-i386 -cdrom out/os.iso -drive file=out/main.img,format=raw,if=virtio -boot d -vga std -serial file:output.txt
+}
+
+function handle_qemu_debug {
+    qemu-system-i386 -drive file=out/main.img,format=raw -cdrom out/os.iso -boot d -vga std -serial file:output.txt -S -s
+}
+
+# Parse the arguments
+
+# Process arguments
+if ($args.Count -eq 0) {
+    Write-Host "Incorrect usage"
+    usage
+} else {
+    $command = $args[0]
+
+    switch ($command) {
+        'docker_get' { handle_docker_get }
+        'build' { handle_build }
+        'clean' { handle_clean }
+        'checkmboot' { handle_checkmboot }
+        'qemu' { handle_qemu }
+        'qemu_debug' { handle_qemu_debug }
+        'help' { usage }
+        default {
+            Write-Host "Incorrect usage"
+            usage
+        }
+    }
+}
