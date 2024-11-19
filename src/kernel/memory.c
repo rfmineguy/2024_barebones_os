6d1c0447fcf523796005784c027774aeb85ad8fa
rfmineguy <rfmineguy@gmail.com>
2024-11-18 20:10:24 -0800

    Memory: Turn off memory information logging for now

--- src/kernel/memory.c
+++ src/kernel/memory.c
@@ -2,10 +2,10 @@
 #include "printf.h"
 #include "log.h"
 
-void memory_init(struct multiboot_info* info) {
-    for (uint32_t i = 0; i < info->mmap_length; i += sizeof(struct multiboot_mmap_entry)) {
-        struct multiboot_mmap_entry *mmmt = (struct multiboot_mmap_entry*)(info->mmap_addr+i);
-        (void)mmmt;
+void memory_init(struct multiboot_header* info) {
+    // for (uint32_t i = 0; i < info->mmap_length; i += sizeof(struct multiboot_mmap_entry)) {
+    //     struct multiboot_mmap_entry *mmmt = (struct multiboot_mmap_entry*)(info->mmap_addr+i);
+    //     (void)mmmt;
 
-    }
+    // }
 }
--- src/kernel/memory.h
+++ src/kernel/memory.h
@@ -1,7 +1,7 @@
 #ifndef MEMORY_H
 #define MEMORY_H
-#include "multiboot.h"
+#include "multiboot2.h"
 
-void memory_init(struct multiboot_info*);
+void memory_init(struct multiboot_header*);
 
 #endif
