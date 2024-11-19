0101035d61cc2e42312c016a291eda2d4c015783
rfmineguy <rfmineguy@gmail.com>
2024-11-18 20:04:42 -0800

    Logging: Improved the logging 'library' to support grouping

---
+++ src/kernel/log.c
@@ -0,0 +1,75 @@
+#include "log.h"
+#include "../stdlib/stdarg.h"
+#include "../stdlib/printf.h"
+#include "../stdlib/string.h"
+
+int group_level = 0;
+int line_length = 0; // used for line groups
+int lines = 0; // used for line groups
+
+void log_group_begin(const char* name){
+    for (int i = 0; i < group_level * 4; i++) serial_printf(" ");
+    serial_printf("[%s]\n", name);
+    group_level++;
+}
+void log_group_end(const char* name){
+    group_level--;
+    for (int i = 0; i < group_level * 4; i++) serial_printf(" ");
+    serial_printf("[%s]\n", name);
+}
+void log_line_begin(const char* name) {
+    line_length = 0;
+    for (int i = 0; i < group_level * 4; i++) serial_printf(" ");
+    serial_printf("[%s] {\n", name);
+}
+void log_line_end(const char* name) {
+    serial_printf("\n");
+    for (int i = 0; i < group_level * 4; i++) serial_printf(" ");
+    serial_printf("} [%s]\n", name);
+}
+void log_info_internal(const char* type, const char* cat, const char* fmt, ...) {
+    //1. format the print into a buffer
+    static char buf[1000] = {0};
+    va_list args;
+    va_start(args, fmt);
+    k_vsprintf(buf, fmt, args);
+    va_end(args);
+
+    //2. format the cat into a buffer
+    static char buf2[250] = {0};
+    k_sprintf(buf2, "[%s]  ", cat);
+
+    //2. print the formatted string on multiple lines (max 50 character line limit)
+    int len = strlen(buf);
+    int lines = len / 50;
+    int index = 0;
+    for (int i = 0; i <= lines; i++) {
+        for (int ix = 0; ix < group_level * 4; ix++) serial_printf(" ");
+        serial_write_str(buf2);
+        while (index < len && buf[index]) {
+            serial_write_ch(buf[index++]);
+            if (i != 0 && index % 50 == 0) break;
+        }
+        serial_write_ch('\n');
+    }
+}
+void log_line_internal(const char* fmt, ...) {
+    static char buf[1000] = {0};
+    va_list args;
+    va_start(args, fmt);
+    k_vsprintf(buf, fmt, args);
+    va_end(args);
+
+    int len = strlen(buf);
+    int index = 0;
+    while (index < len && buf[index]) {
+        if (line_length % 50 == 0)
+            for (int i = 0; i < (group_level + 1) * 4; i++) serial_printf(" ");
+        serial_write_ch(buf[index++]);
+        line_length++;
+        if (line_length % 50 == 0) {
+            serial_write_ch('\n');
+            lines++;
+        }
+    }
+}
--- src/kernel/log.h
+++ src/kernel/log.h
@@ -2,9 +2,35 @@
 #define LOG_H
 #include "serial.h"
 
-#define log_info(cat, fmt, ...) serial_printf("[" cat "][INFO] : " fmt, ##__VA_ARGS__)
-#define log_warn(cat, fmt, ...) serial_printf("[" cat "][WARN] : " fmt, ##__VA_ARGS__)
-#define log_err(cat, fmt, ...) serial_printf("[" cat "][ERR ] : " fmt, ##__VA_ARGS__)
-#define log_crit(cat, fmt, ...) serial_printf("[" cat "][CRIT] : " fmt, ##__VA_ARGS__)
+extern int group_level;
+extern void log_info_internal(const char*, const char*, const char*, ...);
+extern void log_line_internal(const char*, ...);
+
+void log_group_begin(const char*);
+void log_group_end(const char*);
+
+void log_line_begin(const char*);
+void log_line_end(const char*);
+
+// #define log_line_begin(x) log_group_begin(x)
+// #define log_line_end(x) log_group_end(x)
+
+#define log_none(type, cat, fmt, ...) \
+    log_info_internal(type, cat, fmt, ##__VA_ARGS__)
+
+#define LOG_ENABLE
+#ifdef  LOG_ENABLE
+#define log_line(fmt, ...) do { log_line_internal(fmt, ##__VA_ARGS__); } while(0);
+#define log_info(cat, fmt, ...) do { log_none("INFO", cat, fmt, ##__VA_ARGS__); } while(0);
+#define log_warn(cat, fmt, ...) do { log_none("WARN", cat, fmt, ##__VA_ARGS__); } while(0);
+#define log_err(cat, fmt, ...) do { log_none("ERRR", cat, fmt, ##__VA_ARGS__); } while(0);
+#define log_crit(cat, fmt, ...) do { log_none("CRIT", cat, fmt, ##__VA_ARGS__); } while(0);
+#else
+#define log_line(fmt, ...)
+#define log_info(cat, fmt, ...)
+#define log_warn(cat, fmt, ...)
+#define log_err(cat, fmt, ...)
+#define log_crit(cat, fmt, ...)
+#endif
 
 #endif
