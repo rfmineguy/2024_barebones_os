a2851173275152bfad47cecc6d4f02261bdcb2dd
rfmineguy <rfmineguy@gmail.com>
2024-11-18 20:01:34 -0800

    Arena: Added log message to arena to notify when allocations happen

--- src/kernel/arena.c
+++ src/kernel/arena.c
@@ -2,6 +2,8 @@
 #include "log.h"
 
 arena arena_new(uint32_t base_addr, uint32_t limit_addr){
+    log_info("ArenaNew", "created new arena at base_addr=%x, limit_addr=%x\n", 
+            base_addr, limit_addr);
     return (arena) {.base=base_addr, .limit=limit_addr, .used=0};
 }
 
@@ -9,7 +11,7 @@ void* arena_alloc(arena* arena, uint32_t size){
     if (arena->used + size > arena->limit) return (void*)0;
     void* ptr = (void*)(arena->base + arena->used);
     arena->used += size;
-    log_info("ArenaAlloc", "allocated %d bytes at %x\n", size, (uint32_t)ptr);
+    log_info("ArenaAlloc", "allocated %d (0x%x) bytes at %x\n", size, size, ptr);
     return ptr;
 }
 
