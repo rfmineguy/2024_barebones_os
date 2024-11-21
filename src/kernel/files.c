f3fb48c3c2646daeadb801259df5038bd977b2e5
rfmineguy <rfmineguy@gmail.com>
2024-11-20 20:17:50 -0800

    Files: Make the files pane dynamically display the files from the fat drive using the rotodir

--- src/kernel/files.c
+++ src/kernel/files.c
@@ -1,6 +1,16 @@
 #include "files.h"
+#include "fat_drive.h"
 
 void files_populate(ui_box_t* box) {
-    ui_putstr(box, 1, 0, "a.txt");
-    ui_putstr(box, 1, 1, "test.txt");
+    const dir_entry* root_dir = fat_drive_internal_get_root_dir();
+    char filename[11];
+    for (uint32_t i = 1; i < fat_drive_internal_get_boot_sector().DirEntryCount; i++) {
+        if (root_dir[i].Name[0] == 0x00) break;
+
+        fat_drive_8_3_to_filename((const char*)root_dir[i].Name, filename);
+        ui_putstr(box, 1, i - 1, filename);
+    }
+
+    // ui_putstr(box, 1, 0, "a.txt");
+    // ui_putstr(box, 1, 1, "test.txt");
 }
