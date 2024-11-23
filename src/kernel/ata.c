2a875cf4fb47e217726b878483b642bd66dc4300
rfmineguy <rfmineguy@gmail.com>
2024-11-22 17:53:30 -0800

    ATA: Writing sectors now works

--- src/kernel/ata.c
+++ src/kernel/ata.c
@@ -34,9 +34,7 @@ void ata_read(int drive, int lba, uint8_t* buffer, uint32_t sectors){
     ata_select_drive(drive);  // Primary Master (or choose another drive)
 
     // Set the number of sectors to read
-    io_outb(0x1F2, sectors); io_wait();
-
-    // Set the LBA address
+    io_outb(0x1F2, sectors);            io_wait();
     io_outb(0x1F3, lba & 0xFF);         io_wait();  // LBA Low
     io_outb(0x1F4, (lba >> 8) & 0xFF);  io_wait();  // LBA Mid
     io_outb(0x1F5, (lba >> 16) & 0xFF); io_wait();  // LBA High
@@ -56,29 +54,38 @@ void ata_read(int drive, int lba, uint8_t* buffer, uint32_t sectors){
 }
 
 void ata_write(int drive, int lba, uint8_t* buffer, uint32_t sectors) {
+    log_group_begin("ATA Write");
+    log_info("Drive  ", "%x", drive);
+    log_info("LBA    ", "%d", lba);
+    log_info("AbsAddr", "dec (%d), hex(0x%x)", lba * 512, lba * 512);
+    log_info("Sectors", "%d", sectors);
+    log_info("buf_add", "%x", buffer);
+    log_group_end("ATA Write");
+
+    ata_soft_reset();
     ata_wait_ready();
     ata_select_drive(drive);
     io_outb(0x1F2, sectors); io_wait();
+
     // Set the LBA address
     io_outb(0x1F3, lba & 0xFF);         io_wait();  // LBA Low
     io_outb(0x1F4, (lba >> 8) & 0xFF);  io_wait();  // LBA Mid
     io_outb(0x1F5, (lba >> 16) & 0xFF); io_wait();  // LBA High
-    // Send the Write command (0x20)
     io_outb(ATA_STATUS, 0x30); io_wait();  // Write Sectors command
     ata_wait_ready();
 
-    io_inb(0x1F7);
-
     for (uint32_t i = 0; i < sectors * 256; i++) {
+        while (!(io_inb(ATA_STATUS) & ATA_STATUS_DRQ)) {
+            // log_info("Waiting for DRQ", "");
+            if (io_inb(ATA_STATUS) & ATA_STATUS_ERR) { // Handle error
+                uint8_t err = io_inb(ATA_ERROR);
+                log_crit("ATA Write", "Error writing sector %d: ERR=%x\n", lba, err);
+                return;                                 // Exit on error
+            }
+        }
         uint16_t data = (uint16_t)(buffer[i * 2] | (buffer[i * 2 + 1] << 8));  
         io_outw(0x1F0, data);
-
-        uint8_t err = io_inb(ATA_ERROR); io_wait();
-        if (err) {
-            if (err & (1 << 1)) log_crit("ATA Read", "Sector not found: %d\n", lba);
-            if (err & (1 << 3)) log_crit("ATA Read", "Uncorrectable error\n");
-            if (err & (1 << 0)) log_crit("ATA Read", "Bad block detected\n");
-        }
+        // log_info("ATA Write", "wrote %X %X", data & 0xff, (data >> 8) & 0xff);
     }
 }
 
--- src/kernel/ata.h
+++ src/kernel/ata.h
@@ -9,6 +9,11 @@
 
 #define ATA_DRIVE_HEAD 0x1F6
 
+#define ATA_STATUS_ERR  0x1
+#define ATA_STATUS_IDX  0x2
+#define ATA_STATUS_CORR 0x4
+#define ATA_STATUS_DRQ  0x8
+
 void ata_wait_ready();
 void ata_select_drive(uint8_t drive);
 void ata_read (int drive, int lba, uint8_t* buffer, uint32_t sectors);
