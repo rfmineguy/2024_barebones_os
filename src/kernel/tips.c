b6f3529d02e2b1a55a76185b856cffa746bc99c3
rfmineguy <rfmineguy@gmail.com>
2024-12-02 18:49:25 -0800

    Tips: Remove delf from listing

--- src/kernel/tips.c
+++ src/kernel/tips.c
@@ -3,9 +3,9 @@
 void tips_populate(ui_box_t* box) {
     ui_putstr(box, 1, 0, "read <file>");
     ui_putstr(box, 1, 1, "newf <file>");
-    ui_putstr(box, 1, 2, "delf <file>");
-    ui_putstr(box, 1, 3, "appf <file> \"<text>\"");
-    ui_putstr(box, 1, 4, "list");
-    ui_putstr(box, 1, 5, "reboot");
-    ui_putstr(box, 1, 6, "clear");
+    // ui_putstr(box, 1, 2, "delf <file>");
+    ui_putstr(box, 1, 2, "appf <file> \"<text>\"");
+    ui_putstr(box, 1, 3, "list");
+    ui_putstr(box, 1, 4, "reboot");
+    ui_putstr(box, 1, 5, "clear");
 }
