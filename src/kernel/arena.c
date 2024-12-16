32bafce392082a6fdde30055eba4dcd2093001b0
rfmineguy <rfmineguy@gmail.com>
2024-12-15 17:16:01 -0800

    Arena: Log disable

--- src/kernel/arena.c
+++ src/kernel/arena.c
@@ -1,8 +1,9 @@
 #include "arena.h"
+#define LOG_DISABLE
 #include "log.h"
 
 arena arena_new(uint32_t base_addr, uint32_t limit_addr){
-    log_info("ArenaNew", "created new arena at base_addr=%x, limit_addr=%x\n", 
+    log_info("ArenaNew", "created new arena at base_addr=%x, limit_addr=%x", 
             base_addr, limit_addr);
     return (arena) {.base=base_addr, .limit=limit_addr, .used=0};
 }
@@ -11,7 +12,7 @@ void* arena_alloc(arena* arena, uint32_t size){
     if (arena->used + size > arena->limit) return (void*)0;
     void* ptr = (void*)(arena->base + arena->used);
     arena->used += size;
-    log_info("ArenaAlloc", "allocated %d (0x%x) bytes at %x\n", size, size, ptr);
+    log_info("ArenaAlloc", "allocated %d (0x%x) bytes at %x", size, size, ptr);
     return ptr;
 }
 
