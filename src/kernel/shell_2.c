814aabc7b9566a0c58d626603366d736de624f2b
rfmineguy <rfmineguy@gmail.com>
2024-12-21 13:32:49 -0800

    Shell2: Refactored out mouse drawing and management code

--- src/kernel/shell_2.c
+++ src/kernel/shell_2.c
@@ -19,9 +19,9 @@ int shell2_mouse_listener(mouse_device_packet_t mouse_data) {
 	cursor_x += mouse_data.x_difference / 4;
 	cursor_y += mouse_data.y_difference / 4;
 	if (cursor_x <= 0) cursor_x = 0;
-	if (cursor_x >= 77) cursor_x = 77;
+	if (cursor_x >= 79) cursor_x = 79;
 	if (cursor_y <= 0) cursor_y = 0;
-	if (cursor_y >= 22) cursor_y = 22;
+	if (cursor_y >= 24) cursor_y = 24;
 
 	cursor_moved = true;
 
@@ -30,27 +30,35 @@ int shell2_mouse_listener(mouse_device_packet_t mouse_data) {
 
 int shell2_timer_listener(int ticks) {}
 
-int shell2_run(ui_box_t* box) {
+void shell2_show_cursor_data(ui_box_t* box) {
 	static char buf[512];
 
+	k_sprintf(buf, "%d, %d        ", 
+			g_mouse_data.x_delta_signed / 8,
+			g_mouse_data.y_delta_signed / 8);
+	ui_putstr(box, 0, 0, buf);
+	k_sprintf(buf, "%d, %d        ", cursor_x, cursor_y);
+	ui_putstr(box, 0, 1, buf);
+}
+
+void shell2_show_cursor_pos(ui_box_t* box, char* last_frame) {
+	ui_putch_r(box, last_cursor_x, last_cursor_y, *last_frame);
+	*last_frame = ui_getch_r(box, cursor_x, cursor_y);
+	ui_putch_r(box, cursor_x, cursor_y, '#');
+	ui_refresh();
+	cursor_moved = false;
+}
+
+int shell2_run(ui_box_t* box) {
 	cursor_x = 39;
 	cursor_y = 10;
 	ui_box(box);
 
+	char last_frame = ' ';
 	while (1) {
+		shell2_show_cursor_data(box);
 		if (cursor_moved) {
-			k_sprintf(buf, "%d, %d        ", 
-					g_mouse_data.x_delta_signed / 8,
-					g_mouse_data.y_delta_signed / 8);
-			ui_putstr(box, 0, 0, buf);
-			k_sprintf(buf, "%d, %d        ", cursor_x, cursor_y);
-			ui_putstr(box, 0, 1, buf);
-
-			char c = ui_getch(box, last_cursor_x, last_cursor_y);
-			ui_putch(box, last_cursor_x, last_cursor_y, ' ');
-			ui_putch(box, cursor_x, cursor_y, '#');
-			ui_refresh();
-			cursor_moved = false;
+			shell2_show_cursor_pos(box, &last_frame);
 		}
 	}
 }
