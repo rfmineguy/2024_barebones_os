33e852f43bd67f0f7efa162399f28b16a3bdcbae
rfmineguy <rfmineguy@gmail.com>
2024-12-21 12:55:44 -0800

    Mouse: Working mouse driver finally :)

--- src/kernel/mouse.c
+++ src/kernel/mouse.c
@@ -11,9 +11,6 @@
 uint8_t mouse_cycle = 0;
 int8_t  mouse_byte[3];
 
-#define PACKETS_IN_PIPE 1024
-#define DISCARD_POINT 32
-
 #define MOUSE_IRQ 12
 
 #define MOUSE_PORT   0x60
@@ -23,6 +20,9 @@ int8_t  mouse_byte[3];
 #define MOUSE_WRITE  0xD4
 #define MOUSE_F_BIT  0x20
 #define MOUSE_V_BIT  0x08
+
+int (*mouse_listeners[10])(mouse_device_packet_t);
+int mouse_listeners_count;
 
 void mouse_wait(uint8_t a_type) {
 	uint32_t timeout = 100000;
@@ -58,16 +58,53 @@ uint8_t mouse_read() {
 	return t;
 }
 
+void mouse_add_listener(int(*listener)(mouse_device_packet_t)) {
+    if (mouse_listeners_count >= 10) {
+        log_crit("KeyboardAddListener", "Couldn't add new keyboard listener");
+        return;
+    }
+    mouse_listeners[mouse_listeners_count] = listener;
+    mouse_listeners_count++;
+}
 
 void mouse_handler(struct interrupt_registers_test *r) {
-	// log_info("Mouse", "handler called");
 	mouse_byte[mouse_cycle++] = mouse_read();
 
 	if (mouse_cycle == 3) {
+		mouse_cycle = 0;
+		int8_t info = mouse_byte[0];
 		int8_t mouse_dx = mouse_byte[1];
 		int8_t mouse_dy = mouse_byte[2];
-		log_info("Mouse", "DX: %d, DY: %d", mouse_dx, mouse_dy);
-		mouse_cycle = 0;
+
+		if ((info & 0x8) != 0x8) {
+            log_warn("Mouse", "Desynchronized packet. Attempting resync.");
+            
+            // Shift buffer left by one byte and continue
+            mouse_byte[0] = mouse_byte[1];
+            mouse_byte[1] = mouse_byte[2];
+			mouse_cycle = 2;
+            return;
+			// log_info("Mouse", "Something wrong");
+			// mouse_cycle = 0;
+			// return;
+		}
+
+		log_info("Mouse", "Raw [%d, %d, %d]", info, mouse_dx, mouse_dy);
+		for (int i = 0; i < mouse_listeners_count; i++) {
+			int (*listener)(mouse_device_packet_t) = mouse_listeners[i];
+			if (!listener) continue;
+			
+			mouse_device_packet_t pkt = (mouse_device_packet_t) {
+				.x_difference = mouse_dx,
+				.y_difference = -mouse_dy,
+				.x_sign = (info & 0b00001000) ? -1 : 1,
+				.y_sign = (info & 0b00010000) ? -1 : 1,
+				.x_delta_signed = mouse_dx,
+				.y_delta_signed = mouse_dy,
+				.buttons = 0,
+			};
+			listener(pkt);
+		}
 	}
 
 	// if (!(mouse_byte[2] & 0x20)) {
@@ -119,7 +156,7 @@ void mouse_install() {
 	mouse_wait(0);
 	status = io_inb(0x60);
 	if (!(status & (1 << 1))) {
-			log_info("MouseInstall", "Warning: Mouse IRQ not enabled in configuration");
+		log_info("MouseInstall", "Warning: Mouse IRQ not enabled in configuration");
 	}
 
 	// Rest of your initialization...
@@ -157,49 +194,4 @@ void mouse_install() {
 
 	// Install handler and enable interrupts
 	irq_install_handler(MOUSE_IRQ, mouse_handler);
-	// idt_sti();
-
-	/*
-	mouse_wait(1);
-	io_outb(0x64, 0xff);
-
-	mouse_wait(1);
-	io_outb(0x64, 0xA8);
-	mouse_wait(1);
-	io_outb(0x64, 0x20);
-	mouse_wait(0);
-	status = io_inb(0x60) | 2;
-	mouse_wait(1);
-	io_outb(0x64, 0x60);
-	mouse_wait(1);
-	io_outb(0x60, status);
-
-	// setup sample rate
-	// output sped 200, 200, 80
-	mouse_write(0xF3);
-	mouse_read();
-
-	mouse_write(200);
-	mouse_read();
-
-	mouse_write(0xF3);
-	mouse_read();
-
-	mouse_write(200);
-	mouse_read();
-
-	mouse_write(0xF3);
-	mouse_read();
-
-	mouse_write(80);
-	mouse_read();
-
-	mouse_write(0xF6);
-	mouse_read();
-	mouse_write(0xF4);
-	mouse_read();
-
-	irq_install_handler(MOUSE_IRQ, mouse_handler);
-	log_info("MouseInstall", "Initialized PS/2 mouse interface");
-	*/
 }
--- src/kernel/mouse.h
+++ src/kernel/mouse.h
@@ -12,11 +12,18 @@ typedef struct {
 	uint32_t magic;
 	int8_t x_difference;
 	int8_t y_difference;
+	int8_t x_sign;
+	int8_t y_sign;
+
+	int8_t x_delta_signed;
+	int8_t y_delta_signed;
+
 	mouse_click_t buttons;
 } mouse_device_packet_t;
 
 #define MOUSE_MAGIC 0xFEED1234
 
+void mouse_add_listener(int(*)(mouse_device_packet_t));
 void mouse_install();
 
 #endif
