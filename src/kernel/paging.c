aeeeb0f2afe11200819e726aaffd439bc4172939
Riley Fischer <rfmineguy@gmail.com>
2025-10-13 23:07:24 -0700

    [Paging] these functions are responsible for mapping in a cople known memory ranges

--- src/kernel/paging.c
+++ src/kernel/paging.c
@@ -56,3 +56,32 @@ void paging_map_range(uint32_t lo, uint32_t hi, uint32_t flags) {
     last_i = i;
 	}
 }
+
+void paging_unmap_all() {
+	for (int i = 0; i < 1024; i++) {
+		for (int j = 0; j < 1024; j++) {
+			page_tables[i][j] = 0; // present + r/w
+		}
+		page_dir[i] = 0;//((uint32_t)&page_tables[i]) | 0;
+	}
+}
+
+void paging_map_kernel() {
+	log_info("MapKernel", "%x - %x", (uint32_t)&kernel_start, (uint32_t)&kernel_end);
+	paging_map_range((uint32_t)&kernel_start, (uint32_t)&kernel_end, 0x2 | PAGE_PRESENT);
+}
+
+void paging_map_vga() {
+  // vga
+  // each cell is 2 bytes, we have 80 columns and 24 rows
+  // this works out to 2 * 80 * 24 bytes = 3840bytes
+  // starting at 0xb8000
+
+  paging_map_range(0xb8000, 0xb8000 + 0x1000, 0x2 | PAGE_PRESENT);
+}
+
+void paging_map_memory_allocator() {
+  // memory allocator
+  // identity maps arena ranges
+  paging_map_range(0x400000, 0x600000, 0x2 | PAGE_PRESENT);
+}
