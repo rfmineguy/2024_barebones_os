9ea6f89d3eb2bb4bac8029bd92788630f5bb4065
Riley Fischer <rfmineguy@gmail.com>
2025-10-13 23:00:40 -0700

    [Paging] Function for aligning an arbitrary address down to the nearest 'page_size' boundary

--- src/kernel/paging.c
+++ src/kernel/paging.c
@@ -12,3 +12,23 @@ uint32_t page_dir[1024] __attribute__((aligned(4096)));
 uint32_t page_tables[1024][1024] __attribute__((aligned(4096)));
 
 extern uint32_t kernel_start, kernel_end;
+
+/*
+ * ==================================
+ * 0x4387
+ * align(0x4387, 0x4000);
+ *
+ * diff = (0x0387) - 0x4000 
+ * 			= -0x3613
+ *      => (<0)
+ *      => return 0x4387 - 0x0387;
+ *
+ * align(0x61cb88 + 0x1000, 0x1000);
+ * diff = 0x0b88 - 0x1000
+ * 			=> (<0)
+ * 			=> return 0x61db88 - 0x0b88
+ */
+uint32_t paging_align_down(uint32_t addr, uint32_t page_size) {
+	if (page_size == 0) return 0;
+	return (addr - (addr % page_size)) & 0xfffffff000;
+}
