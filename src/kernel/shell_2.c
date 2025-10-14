63b0be4fb584fb26c12590c445afcd9b4161589d
Riley Fischer <rfmineguy@gmail.com>
2025-10-13 23:47:55 -0700

    [Shell] implement consumer of event system and apply it to the shell screen

--- src/kernel/shell_2.c
+++ src/kernel/shell_2.c
@@ -14,29 +14,73 @@ int8_t cursor_x, cursor_y;
 bool cursor_moved, middle_clicked, left_clicked, right_clicked;
 
 void shell2_show_cursor_data(ui_box_t* box) {
-	static char buf[512];
+  static char buf[512];
 
-	ui_putstr(box, 0, 0, buf);
-	k_sprintf(buf, "%d, %d        ", cursor_x, cursor_y);
-	ui_putstr(box, 0, 1, buf);
+  ui_putstr(box, 0, 0, buf);
+  k_sprintf(buf, "%d, %d        ", cursor_x, cursor_y);
+  ui_putstr(box, 0, 1, buf);
 }
 
 void shell2_show_cursor_pos(ui_box_t* box, char* last_frame) {
-	ui_putch_r(box, last_cursor_x, last_cursor_y, *last_frame);
-	*last_frame = ui_getch_r(box, cursor_x, cursor_y);
-	ui_putch_r(box, cursor_x, cursor_y, BLCK);
+  ui_putch_r(box, last_cursor_x, last_cursor_y, *last_frame);
+  *last_frame = ui_getch_r(box, cursor_x, cursor_y);
+  ui_putch_r(box, cursor_x, cursor_y, BLCK);
 }
 
 int shell2_run(ui_box_t* box) {
-	cursor_x = 39;
-	cursor_y = 10;
-	ui_box(box);
+  char buf[500] = {0};
+  int buf_i = 0;
+  cursor_x = 39;
+  cursor_y = 10;
+  frames = 0;
+  ui_box(box);
+  char last_frame = ' ';
 
-	char last_frame = ' ';
-	while (1) {
-		shell2_show_cursor_data(box);
-		if (cursor_moved) {
-			shell2_show_cursor_pos(box, &last_frame);
-		}
-	}
+  while (true) {
+    event e;
+    if (event_system_get(&e)) {
+      event_log(e);
+      switch (e.type) {
+        case KEYBOARD:
+          {
+            log_info("Shell2", "Keyboard");
+            if (e.keyboard.press == 0) {
+              log_info("Shell2", "ch=%d, %c", e.keyboard.ch, e.keyboard.ch);
+              if (e.keyboard.ch == 8) { // backspace
+                if (buf_i > 0) buf[--buf_i] = 0;
+              }
+              else {
+                buf[buf_i++] = e.keyboard.ch;
+                buf[buf_i] = 0;
+              }
+              ui_clear_rv(box, 0, 10, buf_i + 2, 2);
+              ui_putstr(box, 0, 10, buf);
+              ui_putstr(box, strlen(buf), 11, "_");
+            }
+          }
+          break;
+        case MOUSE:
+          {
+            if (e.mouse.moved == 1) {
+              last_cursor_x = cursor_x;
+              last_cursor_y = cursor_y;
+              cursor_x = e.mouse.x;
+              cursor_y = 24 - e.mouse.y;
+              shell2_show_cursor_pos(box, &last_frame);
+              ui_refresh_b(box);
+            }
+
+            if (e.mouse.clicked == 1) {
+              ui_putch(box, e.mouse.x, 24 - e.mouse.y, BLCK);
+            }
+          }
+          break;
+      }
+    }
+    shell2_show_cursor_data(box);
+
+    frames ++;
+    ui_refresh();
+  }
+  return 0;
 }
