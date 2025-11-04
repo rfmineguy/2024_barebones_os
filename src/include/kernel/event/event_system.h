edeb7d9e57ad12ad9cf3d5f067b084df93ea36db
Riley Fischer <rfmineguy@gmail.com>
2025-11-04 13:44:23 -0800

    [Testing] add conditional function for getting the internal event system state

--- src/include/kernel/event/event_system.h
+++ src/include/kernel/event/event_system.h
@@ -1,10 +1,18 @@
 #ifndef EVENT_SYSTEM_H
 #define EVENT_SYSTEM_H
 #include "event.h"
+#ifdef TEST_ENABLE
+#include "datastructures/queue_event.h"
+#endif
 
 void event_system_init();
 void event_system_deinit();
 void event_system_post(event e);
 int  event_system_get(event* e);
+
+#ifdef TEST_ENABLE
+const queue_event* event_system_get_event_queue();
+#endif
+
 
 #endif
